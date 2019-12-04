e5165bc926b0014f3f79fe97321eff70420efc72
luis fernando flores arriaga <luis.flores@ironbit.com.mx>
2019-12-04 15:11:41 -0600

    fix(index) fixin the returns

--- gotama97/practica-2019-12-04/Kata/index.js
+++ gotama97/practica-2019-12-04/Kata/index.js
@@ -1,75 +1,74 @@
-  function findMissingLetter(array){
+    function findMissingLetter(array){
 
-    let arrayCap = [];
-    let arrayLow=[];
-    let position=0;
-    let position1=0;
+        let arrayCap = [];
+        let arrayLow=[];
+        let position=0;
+        let missingLetter = "";
 
-    //MAPEANDO EL ARREGLO DE MAYUSCULAS
-    for(let i = 0;i<26;i++){
-        let letter = String.fromCharCode(i+65);
-        arrayCap[i]=letter;
-    }
-//charCodeAt(number);
-    //MAPEANDO EL ARREGLO DE MINUSCULAS
-    for(let i = 0;i<26;i++){
-        let letter = String.fromCharCode(i+97);
-        arrayLow[i]=letter;
-    }
+        //MAPEANDO EL ARREGLO DE MAYUSCULAS
+        for(let i = 0;i<26;i++){
+            let letter = String.fromCharCode(i+65);
+            arrayCap[i]=letter;
+        }
+    //charCodeAt(number);
+        //MAPEANDO EL ARREGLO DE MINUSCULAS
+        for(let i = 0;i<26;i++){
+            let letter = String.fromCharCode(i+97);
+            arrayLow[i]=letter;
+        }
 
-    //CALLBACKS
-    const confirmation = (cur,idx)=>{
-        if(cur===array[0]){
-            position=idx;
-            return true;
-        }return false;
-    }
-
-    const confLow = (cur,idx)=>{
-        if(cur==array[0]){
-            position=idx;
-            return true
-        }return false
-    }
-
-    const compCap = (cur,idx)=>{
-        if(cur[idx]===array[idx]){
-            return true;
-        }else {
-            position1=idx;
+        //CALLBACKS
+        const confirmation = (cur,idx)=>{
+            if(cur===array[0]){
+                position=idx;
+                return true;
+            }
             return false;
         }
-    }
 
-    const compLow=(cur,idx)=>{
-        if(cur[idx]!=array[idx]){
-            return true;
-        }else{
-            position=idx;
+        const confirmationlow = (cur,idx)=>{
+            if(cur===array[0]){
+                position=idx;
+                return true;
+            }
             return false;
         }
-    }
+    
+    
+
+        if(arrayCap.some(confirmation)){
+            
+            let arrayComp = arrayCap.slice(position,(array.length+1+position));
+            //REALIZANDO LA COMPRACION DE LOS DOS ARREGLOS
+            console.log(arrayComp);
+            for (let i = 0; i < array.length; i++){
+                if(arrayComp[i] === array[i]){
+                    
+                } else{
+                    missingLetter = arrayComp[i];
+                    return missingLetter;
+                }
+            }
+        }else if( arrayLow.some(confirmationlow)){
+            let arrayComp = arrayLow.slice(position,(array.length+1+position));
+            //REALIZANDO LA COMPRACION DE LOS ARRGLOS
+            console.log(arrayComp);
+            for (let i = 0; i < array.length; i++){
+                if(arrayComp[i] === array[i]){
+                
+                } else{
+                    missingLetter = arrayComp[i];
+                    return missingLetter;
+                }
+            }
+        }
 
 
-    if(arrayCap.some(confirmation)){
         
-        let arrayComp = arrayCap.slice(position,(array.length+1));
-        //REALIZANDO LA COMPRACION DE LOS DOS ARREGLOS
-        if(arrayComp.some(compCap)){
-            return arrayComp[position1];
-        }
-
-    }else{
-        let arrayComp = arrayLow.slice(position,(array.length+1));
-        //REALIZANDO LA COMPRACION DE LOS ARRGLOS
-        if(arrayComp.some(compLow)){
-            return arrayComp[position1];
-        }
+        console.log(missingLetter);
+    return missingLetter;
     }
 
-  return ' ';
-}
+let array=["p","q","s","t"];
 
-let array=["B","C","D","F"];
-
-findMissingLetter(array);
+console.log(findMissingLetter(array));
