6c9dd2d01c9e18aea8607fd3d407c742046b96aa
Analin Flores <foglzerika@gmail.com>
2019-12-06 15:13:55 -0600

    Adding post method to users#

--- Any28Flo/051219/frontend/assets/js/showUser.js
+++ Any28Flo/051219/frontend/assets/js/showUser.js
@@ -1,4 +1,5 @@
 let urlUser ="http://localhost:3000/users/"
+let addUser = document.getElementById("createUser");
 
 //Async ensures that the function returns a promise
 //and wraps non-promises in it.
@@ -16,18 +17,42 @@ const makeRequest = async (urlUser)  =>{
    
    
 }
+const addUserToDB = ()=>{
+    console.log("Adding a user");
+    
+    let firstName= $("#nameUser");
+    let nick = $("#nickName");
+
+    let lastName ="Glz";
+    let userName = nick[0].value;
+    let password = "dfadfsadfss";
+
+    (async () => {
+        const rawResponse = await fetch(urlUser, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({id:1, firstName: firstName, lastName: lastName , userName : userName , password : password})
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+      })();
+}
 const printUsers = jsonData =>{
     let userContainer = document.querySelector("#users");
     let html ="";    
     for(let user of jsonData){
         console.log(user.firstName)
            html += `<div>`
-            html += `<div class="card col-md-2" data-card-name="${user.firstName}">`;
+            html += `<div class="card col-12" data-card-name="${user.firstName}">`;
             html += `<div class="card-body"> `
                 html += `<h5 class= "card-title">${user.firstName} ${user.lastName} </h5>`
                 html += `<p class= "card-text">User Name : ${user.userName} </p>`
             html += `</div> `
-            html+= `  <a href="#" class="btn btn-primary">Update</a>`
+            html+= `  <a href="#" class="btn btn-primary">Delete</a>`
             html += `</div>`
            html += `</div>`
            console.log(user)
@@ -39,3 +64,4 @@ const printUsers = jsonData =>{
 
 makeRequest(urlUser);
   
+addUser.onclick = addUserToDB;
