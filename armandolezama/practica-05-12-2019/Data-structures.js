e2aaf03055e774c9126da23cdc34841095883201
armandolezama <victor.lezama@ironbit.com.mx>
2019-12-11 17:58:28 -0600

    Fix(All methods and structures are completed)

--- armandolezama/practica-05-12-2019/Data-structures.js
+++ armandolezama/practica-05-12-2019/Data-structures.js
@@ -30,36 +30,143 @@ class Stack {
     };
 }
 
-class linkedList {
-    constructor(){
-        this.head = null;
-        this.tail = null;
-    }
-
-    insertion(){
-        let node = new Node
-        this.head = node
-    }
-
-    deletion(){
-
-    }
-
-    display(){
-
-    }
-    search(){
-
-    }
-    delete(){
-
+class Node {
+    constructor (data){
+        this.next = null;
+        this.prev = null;
+        this.data = data;
     }
 
 }
 
-class Node {
-    constructor(field, reference){
-        this.field = field;
-        this.reference = reference 
+class DoubleLinkedList {
+    constructor(){
+        this.list = new Set();
+        this.currentNode = null;
     }
+
+    add(data, prev = null, next = null) {
+        const node = new Node(data);
+        node.prev = prev;
+        node.next = next;
+        if (prev !== null ){
+            prev.next = node;
+        }
+
+        if (next !== null){
+            next.prev = node;
+        }
+
+        this.currentNode = this.currentNode === null ? node : this.currentNode;
+        this.list.add(node);
+    }
+
+    prev(){
+        this.currentNode = this.currentNode.prev !== null ? this.currentNode.prev : this.currentNode;
+    }
+
+    next(){
+        this.currentNode = this.currentNode.next !== null ? this.currentNode.next : this.currentNode;
+    }
+
+    delete(){
+        if (this.currentNode.prev !== null ){
+            this.currentNode.prev.next = this.currentNode.next;
+        }
+
+        if (this.currentNode.next !== null){
+            this.currentNode.next.prev = this.currentNode.prev;
+        }
+        
+        this.list.delete(this.currentNode);
+        if(this.currentNode.prev !== null){
+            this.currentNode = this.currentNode.prev;
+        } else {
+            this.currentNode = this.currentNode.next
+        }
+    }
+
+}
+
+class LinkedList {
+    constructor(){
+        this.list = new Set();
+        this.currentNode = null;
+        this.head = null;
+    }
+
+    add(data, prev = null, next = null) {
+        const node = new Node(data);
+        node.prev = prev;
+        node.next = next;
+        if (prev !== null ){
+            prev.next = node;
+        } else {
+            this.head = node;
+        }
+
+        if (next !== null){
+            next.prev = node;
+        }
+
+        this.currentNode = this.currentNode === null ? node : this.currentNode;
+        this.list.add(node);
+    }
+
+    next(){
+        this.currentNode = this.currentNode.next !== null ? this.currentNode.next : this.currentNode;
+    }
+
+    delete(){
+        if (this.currentNode.prev !== null ){
+            this.currentNode.prev.next = this.currentNode.next;
+        }
+
+        if (this.currentNode.next !== null){
+            this.currentNode.next.prev = this.currentNode.prev;
+        }
+        
+        this.list.delete(this.currentNode);
+        this.currentNode = this.currentNode.next;
+    }
+
+    backToHead(){
+        this.currentNode = this.head;
+    }
+}
+
+
+class Tail {
+    constructor() {
+        this.stack = [];
+        this.typeOfData = '';
+    };
+
+    push(newElement){
+        if(typeof newElement === typeof this.stack[0] || this.stack.length === 0 ){
+            this.stack = [newElement, ...this.stack]
+        } else {
+            console.error('New element is not same type of stack')
+        }
+    };
+
+    removeFromTail(){
+        if(this.stack.length > 0){
+            this.stack = [...this.stack.slice(1, this.stack.length)]
+        } else {
+            console.error('Current stack is empty')
+        }
+    };
+
+    removeFromHead(){
+
+    }
+
+    get(){
+        if(this.stack.length > 0){
+            return this.stack[0]
+        } else {
+            console.error('Current stack is empty')
+        }
+    };
 }
