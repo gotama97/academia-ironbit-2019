30080b21d14e0e026e635e4e6a23136df225caca
armandolezama <victor.lezama@ironbit.com.mx>
2019-12-11 23:02:10 -0600

    Fix(Subjects class now can assign hours and subjects to employees of school)

--- armandolezama/practica-10-12-2019/SQLita/Subjects.js
+++ armandolezama/practica-10-12-2019/SQLita/Subjects.js
@@ -5,9 +5,22 @@ export class Subjects {
 
     };
 
-    setClass(){}
-    setBreak(){}
-    setMeetings(){}
-    setAdministrativeWork(){}
+    setClass(teacher, hour, subject, day){
+        teacher.__schedule[day].occupyHour(hour, subject);
+    }
+    setBreak(employee, hour, day){
+        employee.__schedule[day].occupyHour(hour, 'Break');
+    }
+    setMeetings(administrative, startTime, day){
+        administrative.__schedule[day].occupyHour(startTime, 'Meeting');
+        administrative.__schedule[day].occupyHour(startTime + 1, 'Meeting');
+        administrative.__schedule[day].occupyHour(startTime + 2, 'Meeting');
+    }
+    setAdministrativeWork(administrative, day){
+        administrative.__schedule[day].occupyHour(startTime, 'Administrative Work');
+        administrative.__schedule[day].occupyHour(startTime + 1, 'Administrative Work');
+        administrative.__schedule[day].occupyHour(startTime + 2, 'Administrative Work');
+        administrative.__schedule[day].occupyHour(startTime + 3, 'Administrative Work');
+    }
 
 };
