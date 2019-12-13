55936be70e2c577222d3867b3742d372ca8a2247
Manuel García <Lobos@MacBook-Adry-y-Manu.local>
2019-12-13 13:13:19 -0600

    feat: change es5 functions to arrow function

--- manuelEnriqueIronbit/practica-2019-12-09/phoneNumberTesting/src/function-test/index.js
+++ manuelEnriqueIronbit/practica-2019-12-09/phoneNumberTesting/src/function-test/index.js
@@ -1,45 +1,11 @@
-const alphabetPosition = text => {
-  let arrayCap = [];
-  let arrayLow = [];
-  //MAPEANDO EL ARREGLO DE MAYUSCULAS
-  for(let i = 0;i<26;i++){
-      let letter = String.fromCharCode(i+65);
-      arrayCap[i]=letter;
-  }
-  //MAPEANDO EL ARREGLO DE MINUSCULAS
-  for(let i = 0;i<26;i++){
-      let letter = String.fromCharCode(i+97);
-      arrayLow[i]=letter;
-  }
-
-  //MAPEANDO EL ARREGLO DE MINUSCULAS
-  //DECONSTRUYENDO EL ARRAY EN CARACTERES
-  let phraseArray = Array.from(text);
-  //IGNORANDO LOS ESPACIOS DEL ARREGLO 
-  let ignoreSpaces = phraseArray.filter(c=>c!=' ');
-  console.log(ignoreSpaces);
-  
-  for (element of ignoreSpaces){
-    if(typeof element !== 'string'){
-      return "Only strings can be used";
+const createPhoneNumber = numbers => {
+  //Se separan los elementos deseados con slice
+  for(element of numbers){
+    if (typeof element !== "number"){
+      return 'All elements need to be numbers'
     }
   }
-
-  //CAMBIANDO LETRAS A NUMEROS
-  let res = ignoreSpaces.map((cur,idx)=>{
-      for(let i=0;i<26;i++){
-          if(cur===arrayLow[i]||cur===arrayCap[i]){
-              return (cur=(i+1))
-          }
-      }
-  });
-  let res2 = res.filter(c=>c!=undefined).join(' ');
-  test=res2;
-  return test;
+  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
 }
 
-let test="42 is the answer.";
-
-alphabetPosition(test);
-
-module.exports = alphabetPosition;
+module.exports = createPhoneNumber;
