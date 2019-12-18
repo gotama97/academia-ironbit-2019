f7e62b0c90aadded4df59dfecf34d49df3e380eb
luis fernando flores arriaga <luis.flores@ironbit.com.mx>
2019-12-18 09:54:50 -0600

    update

--- gotama97/practica-2019-12-17/suma/my-element.js
+++ gotama97/practica-2019-12-17/suma/my-element.js
@@ -1,4 +1,6 @@
-import {LitElement, html} from 'lit-element';
+import {LitElement, html,css} from 'lit-element';
+
+import '@vaadin/vaadin-text-field/vaadin-text-field'
 
 class MyElement extends LitElement{
     constructor(){
@@ -6,10 +8,34 @@ class MyElement extends LitElement{
         this.a = 0;
         this.b = 0;
         this.c= 0;
+        this.numbers = [];
+        this.title='HOLA SUMA';
+    }
+    static get properties() {
+        return {
+            numbers: Array,
+            title: String,
+            a: Number,
+            b: Number,
+            c: Number
+        }
+    }
+
+    static get styles(){
+        return css`
+            h2 {
+                color: blue;
+            }
+        `;
     }
 
     render(){
         return html`
+
+        <h2>${this.title}</h2>
+        
+        <vaadin-text-field placeholder="a" @input="${this.aChanged}"></vaadin-text-field>
+        <vaadin-text-field placeholder="b" @input="${this.bChanged}"></vaadin-text-field>
             <table>
                 <tr>
                     <th>1</th>
@@ -23,9 +49,42 @@ class MyElement extends LitElement{
                     <th>9</th>
                     <th>10</th>
                 </tr>
-            </table>
-        `;
+                <tbody>
+                    <tr>
+                        ${this.suma()}
+                    ${this.numbers.map(number => html`<td>${number}</td>`)}
+                    </tr>
+                </tbody>
+            </table>`
     }
+
+    suma(){
+        this.c=Number(this.a) + Number(this.b);
+        this.multiplyNumber();
+    }
+    
+
+    multiplyNumber(){
+        this.numbers = [];
+
+        for(let i =1;i<=10;i++){
+            this.numbers = [...this.numbers,(this.c*i)]
+        }
+    }
+    aChanged(event){
+        this.a =event.target.value;
+        console.log(event.target.value);
+        this.suma();
+        
+    }
+
+    bChanged(event){
+        this.b = event.target.value;
+        console.log(event.target.value);
+        this.suma();
+    }
+
+ 
 }
 
 window.customElements.define('my-element',MyElement);
